0c898099f8f7ed9663b935183d15520ff816f168
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2024-01-02 18:44:12 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        int[] data = new int[10];
+public class RandomDataGenerator {
+
+    }
+        }
+}
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
